11779ed0cd9e328b8da672dad0f4112dddabbdef
ActionPHP <giosis2e@yahoo.co.uk>
2015-01-06 18:14:35 +0000

    Added foreach loop, and made several other changes

--- ineditor.js
+++ ineditor.js
@@ -1,15 +1,26 @@
 $.fn.ineditor = function(options,callback){
+var callback = callback;
+var options = options;
+return this.each(function(){
 
 
-		this.click(function(){
-			
+		var _content = $(this).html();
+		if(!_content && _content != '0'){
+			$(this).html('Click to edit');
+		}
+		
+		$(this).click(function(){
+
 			var _edited = $(this);
 
 			if(!_edited.hasClass('actionphp-ineditor')) {
 					
-					
 					var _value = _edited.html();
+
+					_value = (_value == 'Click to edit') ? '' : _value;
+
 					var _original = _value;
+					var _id = _edited.attr("id");
 
 					var _input = '<input type="text" value="' + _value + '" class="actionphp-ineditor-input" />';
 
@@ -19,17 +30,30 @@ $.fn.ineditor = function(options,callback){
 
 					var _edited_input = _edited.find('.actionphp-ineditor-input');
 					
-					_edited_input.blur(function(){
+					_edited_input.focusout(function(){
 					
 						var _edited_value = _edited_input.val();
+
+						if(!_edited_value){
+
+							_edited_value = 'Click to edit';
+						}
+
 						_edited_value = (_edited_value) ? _edited_value : _original;
+						
+
 						_edited.html(_edited_value);
 					
 						_edited.removeClass('actionphp-ineditor');
 
-
+						var _data = {
+							_this: this,
+							original_value: _original,
+							new_value: _edited_value,
+							id : _id
+						}
 						if (typeof callback == 'function') { // make sure the callback is a function
-				     	   callback.call(this); // brings the scope to the callback
+				     	   callback.call(this,_data); // brings the scope to the callback
 				    	}
 
 					});
@@ -39,21 +63,22 @@ $.fn.ineditor = function(options,callback){
 
 			if(e.keyCode == 13){
 
-				$('.actionphp-ineditor-input').trigger('blur');
+				_edited.find('.actionphp-ineditor-input').trigger('focusout');
 			}
 
 			});
 
 		});
 
-		
+	
+});	
 
 		$(document).click(function(e){
 			//	console.log(this);
 				//console.log(e.target);
 				if(!$(e.target).hasClass('actionphp-ineditor-input') && !$(e.target).hasClass('actionphp-ineditor') ){
 
-					$('.actionphp-ineditor-input').trigger('blur');
+					$('.actionphp-ineditor-input').trigger('focusout');
 				}
 			});
 
